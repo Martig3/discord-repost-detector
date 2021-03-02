8c063254e6f8eb9cdc32d61b3d7f5e422d4c8846
MartinWienc <32913968+MartinWienc@users.noreply.github.com>
2021-03-01 23:25:29 -0500

    added support for links, updated README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # discord-repost-detector
-Discord bot that detects reposted images
+Rust based Discord bot that detects reposted images & links.
+
+## Config
+Set the following environment variables:
+```
+REPOST_DISCORD_TOKEN=your_discord_bot_token
+REPOST_CACHE_LIMIT=5000
+```
+`REPOST_CACHE_LIMIT` can be set to any 64bit unsigned integer.
+
+## Run
+
+Standard `cargo run`
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use std::collections::HashSet;
 use std::env;
 
 use chrono::{DateTime, Utc};
 use image;
 use img_hash::{HasherConfig, ImageHash};
+use regex::Regex;
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
 use serenity::framework::standard::StandardFramework;
 use serenity::model::channel::{Attachment, Message};
 use serenity::model::user::User;
 use serenity::prelude::TypeMapKey;
-use serenity::utils::{MessageBuilder};
+use serenity::utils::MessageBuilder;
 
 struct Handler;
 
@@ -20,14 +22,27 @@ struct ImageMetadata {
     msg_link: String,
 }
 
-struct Metadatacache;
+struct LinkMetadata {
+    url: String,
+    timestamp: DateTime<Utc>,
+    user: User,
+    msg_link: String,
+}
+
+struct HashCache;
+
+struct LinkCache;
 
 struct Config {
     cache_limit: u64,
 }
 
-impl TypeMapKey for Metadatacache {
+impl TypeMapKey for HashCache {
     type Value = Vec<ImageMetadata>;
+}
+
+impl TypeMapKey for LinkCache {
+    type Value = Vec<LinkMetadata>;
 }
 
 impl TypeMapKey for Config {
@@ -46,7 +61,8 @@ async fn main() {
         .expect("Error creating client");
     {
         let mut data = client.data.write().await;
-        data.insert::<Metadatacache>(Vec::new());
+        data.insert::<HashCache>(Vec::new());
+        data.insert::<LinkCache>(Vec::new());
         data.insert::<Config>(read_config());
     }
     // start listening for events by starting a single shard
@@ -64,21 +80,62 @@ fn read_config() -> Config {
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, context: Context, msg: Message) {
+        if msg.author.bot { return; }
+        let url_regex = Regex::new("https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\\.[a-z]{2,4}\\b([-a-zA-Z0-9@:%_+.~#?&/=]*)").unwrap();
         let attachments: Vec<&Attachment> = msg.attachments.iter().map(|a| a).collect();
         let embedded: Vec<String> = msg.embeds.iter()
             .map(|e| e.url.clone())
             .filter(|e| e.is_some())
             .map(|e| e.unwrap().clone())
             .collect();
-        if attachments.is_empty() && embedded.is_empty() {
+        if attachments.is_empty() && embedded.is_empty() && !url_regex.is_match(&msg.content) {
             return;
+        }
+        let mut data = context.data.write().await;
+        let config: &Config = data.get::<Config>().unwrap().clone();
+        let cache_size = config.cache_limit.clone();
+        let link_cache: &mut Vec<LinkMetadata> = &mut data.get_mut::<LinkCache>().unwrap();
+        let url_matches: HashSet<&str> = url_regex.find_iter(&msg.content).map(|mat| mat.as_str()).collect();
+        for url in url_matches {
+            let link = link_cache.iter().find(|l| l.url == url);
+            if link.is_some() {
+                let utc_now: DateTime<Utc> = Utc::now();
+                let days_between = utc_now.signed_duration_since(link.unwrap().timestamp).num_days();
+                let mut days_between_str = String::from(" posted this ");
+                if days_between < 1 {
+                    days_between_str.push_str("earlier today: ");
+                } else {
+                    days_between_str.push_str(&*format!("{} days ago: ", days_between));
+                }
+                let msg_content = MessageBuilder::new()
+                    .push("That's a repost! ")
+                    .mention(&link.unwrap().user)
+                    .push(days_between_str)
+                    .push(&link.unwrap().msg_link)
+                    .build();
+                if let Err(e) = msg.reply_mention(&context.http, msg_content).await {
+                    println!("{}", e);
+                }
+            } else {
+                let url = url.to_string();
+                let user = msg.author.clone();
+                let msg_link = msg.link().clone();
+                let timestamp = msg.timestamp;
+                println!("{}", url);
+                link_cache.push(LinkMetadata { url, timestamp, user, msg_link });
+                if link_cache.len() > cache_size as usize {
+                    link_cache.pop();
+                }
+            }
         }
         let mut hashes: Vec<ImageHash> = Vec::new();
         for url in embedded {
             if let Ok(resp) = reqwest::get(&url).await {
                 if let Ok(img_bytes) = resp.bytes().await {
-                    let image_hash = HasherConfig::new().to_hasher().hash_image(&image::load_from_memory(img_bytes.as_ref()).unwrap());
-                    hashes.push(image_hash);
+                    if let Ok(img) = &image::load_from_memory(img_bytes.as_ref()) {
+                        let image_hash = HasherConfig::new().to_hasher().hash_image(img);
+                        hashes.push(image_hash);
+                    }
                 }
             }
         }
@@ -88,11 +145,8 @@ impl EventHandler for Handler {
                 hashes.push(image_hash);
             }
         }
-        let mut data = context.data.write().await;
-        let config: &Config = data.get::<Config>().unwrap().clone();
-        let cache_size = config.cache_limit.clone();
         let mut result: Option<&ImageMetadata>;
-        let metadata_cache: &mut Vec<ImageMetadata> = &mut data.get_mut::<Metadatacache>().unwrap();
+        let metadata_cache: &mut Vec<ImageMetadata> = &mut data.get_mut::<HashCache>().unwrap();
         for hash in hashes {
             result = metadata_cache.iter()
                 .find(|i| hash.dist(&i.hash) < 2);
